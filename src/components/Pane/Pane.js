1e2719bd3a95e55b174ca43d7583c72bce35b613
coderZel0 <obitouchiha0026@gmail.com>
2022-06-12 19:37:40 +0530

    worked on conetextMenu

--- src/components/Pane/Pane.js
+++ src/components/Pane/Pane.js
@@ -1,22 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect ,useRef} from 'react';
 import Note from '../note/Note';
 import './Pane.css';
+
 
 
 const Pane = ({paneTitle:{Title},notes,style})=> {
     
     const {title,primary} = style;
+    const menuRef = useRef();
 
     const showOptions = (e)=>{
-        if(e.button === 1) console.log("cmenu");
-        const clickX = e.nativeEvent.offsetX;
-        const clickY = e.nativeEvent.offsetY;
+        //e.preventDefault();
+        console.log("right clicked");
+        const clickX = e.clientX;
+        const clickY = e.clientY;
         console.log(clickX)
+        console.log(e);
     }
+
+    
+
     useEffect(()=>{
+
         document.addEventListener("contextmenu",(e)=>{
             e.preventDefault();
-            console.log(e.pageX);
+           
         })
         return ()=>{
             document.removeEventListener("contextmenu",(e)=>{
@@ -26,8 +34,8 @@ const Pane = ({paneTitle:{Title},notes,style})=> {
     },[])
 
     return(
-        <div className="pane">
-            <div onContextMenu={(e)=>{showOptions(e)}} className='pane__layer'></div>
+        <div onContextMenu={(e)=>{showOptions(e)}} className="pane">
+            <div ref={menuRef} onClick={(e)=>{showOptions(e)}} className='pane__layer'></div>
             <div className="pane__title" style={{background:`radial-gradient(${title.gradient1},${title.gradient2})`,color:`${primary}`}}>{Title}</div>
             <div className="pane__content">
                 <div className='content__inner'>
--- src/components/note/Note.js
+++ src/components/note/Note.js
@@ -1,10 +1,11 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState,useEffect, useRef } from 'react'
 import './note.css';
 
 const Note = ({note,style}) => {
 const [title,setTitle] = useState('');
 const [content,setContent] = useState('');
 
+const menu = useRef(null);
 
 useEffect(()=>{
 if(!note) return;
@@ -14,11 +15,19 @@ setTitle(title);
 setContent(description);
 
 },[note])
-    
+
+function handleRightClick(e){
+  console.log("riight clickl keli re bhau")
+}
+
+useEffect(()=>{
+  
+},[])
+
   return (
     <div className='note' style={{background:`radial-gradient(${style.gradient1} 20%,${style.gradient2}) 100%`}}>
         <div className='note__layer'></div>
-        <div className='note__content'>
+        <div ref={menu} onContextMenu={(e)=>{console.log("right click")}} className='note__content'>
             <div className='note__title'>{title}</div>
             <div className='note__description'>
                 {content}
